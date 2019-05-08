2bb2235060a119cea86fb37c0b9f483da8ea679a
Neotoxic <44700383+Neotoxic-off@users.noreply.github.com>
2019-05-08 21:45:29 +0200

    Add files via upload

---
+++ Bash/INSTALLER.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#---------------
+# INSTALLER
+#---------------
+
+# The folder /bin is the folder where all the commands are located
+# so if you copy your own program in it, you can call it from every where
+
+# INSTALLER
+
+missing(){
+    printf "FILE NOT FOUND\n"
+    exit
+}
+
+installer(){
+
+    # Check if the file to copy exists
+    if [[ -f $1 ]]; then
+        printf "[+] File found\n"
+
+        # Adding the right to execute the file
+        chmod +x $1
+
+
+        # Check if the program is already installed
+        if [[ -f "/bin/$1" ]]; then
+            printf "[+] Script already installed\n"
+
+            # Asking for update or no
+            printf "\n[+] Do you want to update ? [ y / n ]"
+            read update
+
+            if [[ $update == 'y' ]]; then
+                printf "Updating\n"
+
+                # Removing the old file
+                sudo rm /bin/$1
+                cp $1 /bin/
+            else
+                printf "Don't update\n"
+                exit
+            fi
+
+        else
+            printf "[+] Installing\n"
+            cp $1 /bin/
+        fi
+
+        # Check if the file is now into the folder /bin
+        if [[ -f "/bin/$1" ]]; then
+            printf "[+] Installation successful\n"
+            exit
+        fi
+    else
+        missing
+    fi
+}
+
+installer $1
---
+++ Bash/Summary.md
@@ -0,0 +1,23 @@
+# BASHIT
+
+![](https://img.shields.io/badge/Free%20Course-red.svg)
+
+# Learn Bash with examples
+
+* **Linux Bases** : Learn the bases of linux for your scripts [ *cd* / *./* / *ls* / *chmod* / *rm* / *cp* ]
+* **The Redirections** : How to copy a file's content into an other in -1s ? [ *cat* / *echo* / *>* / *>>* ]
+
+
+# Algorithm
+
+* **The Variables** : Manipulation of variables with numbers and characters [ *$var* / *printf* / *read* ]
+* **The IF** : Learn how to check the equality [ *if* / *else* ]
+* **The WHILE** : Learn how to use the While and create loops [ *while* / *++* / *--* ]
+* **The Functions** : Use the functions and the arguments to get a clear code [ *functions()* / *$1* *$2* ]
+* **Installer** : Create an installer for your programm [ */bin* ]
+
+
+*Author : Neotoxic*
+
+
+![](https://static1.squarespace.com/static/532b290de4b0eaed9ab559ae/t/5a737a134192020b71e8f3b9/1553117779995/LearnMore-01.png)
